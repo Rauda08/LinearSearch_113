dbfd76a2cbafafa0ddeec50383ee6d927712df3d
rauda08 <raudajnnh08@gmail.com>
2023-04-13 14:45:45 +0700

    menambahkan element array

--- LinearSearchC_113/LinearSearchC_113.cpp
+++ LinearSearchC_113/LinearSearchC_113.cpp
@@ -13,9 +13,19 @@ void input()
         cout << "Enter the number of elements in the array: ";
         cin >> n;
         if ((n > 0) && (n <= 20))
-            break; 
+            break;
+        else
+            cout << "\Array should have minimum 1 and maximum 20 elements.\n\n";
+    }
 
-
+    // Accept array elements
+    cout << "\n---------------------------\n";
+    cout << " Enter array elements \n";
+    cout << "-----------------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">" ";
+            cin >> arr[i];
     }
 }
 
